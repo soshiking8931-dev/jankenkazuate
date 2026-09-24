5982fe8ce48f1cf3608d5ab16c9dfe0c1d0fb9d1
soshi_0724@icloud.com <soshi_0724@icloud.com>
2026-09-24 13:27:52 +0900

    aiueo

--- jankenkazuate/jankenkazuate.cpp
+++ jankenkazuate/jankenkazuate.cpp
@@ -2,10 +2,12 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
     std::cout << "Hello World!\n";
+    cout << "aiueo";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
